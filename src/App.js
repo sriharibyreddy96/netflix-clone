782d4f0e12cf153c6e2fe3d92f294fd2a82d2cfe
sriharibyreddy96 <sriharibyreddy96@gmail.com>
2025-02-06 09:46:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Signup from './Pages/Signup/Signup';
-import Pricing from './Pages/Pricing/Pricing';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Pricing from "./Pages/Pricing/Pricing";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/pricing' element={<Pricing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
       </BrowserRouter>
     </>
